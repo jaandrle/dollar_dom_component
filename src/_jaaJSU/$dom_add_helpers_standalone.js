5f0c55c359a2a2493d1ee8fad844adba059229c2
Jan Andrle <andrle.jan@centrum.cz>
2019-08-22 18:36:56 +0200

    $dom.component@1.0.0

--- src/_jaaJSU/$dom_add_helpers_standalone.js
+++ src/_jaaJSU/$dom_add_helpers_standalone.js
@@ -1,5 +1,4 @@
 /* jshint esversion: 6,-W097, -W040, browser: true, expr: true, undef: true */
-/* core.js *//* global parseHTML, c_CMD, active_page, __internal_switch_values_holder *///gulp.keep.line
 /* $dom *//* global $dom */
 /* standalone= gulp_place("app.standalone='standalone'", "variable"); */
 gulp_place("$dom_component.sub.js");/* global gulp_place */

a85cbe708348c665f7a394d357f6ae312ff256ff
Jan Andrle <andrle.jan@centrum.cz>
2019-08-22 14:21:51 +0200

    Updated documentation for `update`

--- bin/_jaaJSU/$dom_add_helpers_standalone.sub.js
+++ bin/_jaaJSU/$dom_add_helpers_standalone.sub.js
@@ -366,23 +366,30 @@ $dom.component= function(el_name, attrs, { mapUpdate }={}){
      * Method updates all registered varibles by keys `onupdates` and calls follower functions
      * @method update
      * @public
-     * @param {Object} new_data
+     * @param {Object|Function} new_data
      *  - When `$dom.component` is initialized, it is possible to register `mapUpdate`
      *  - **It's because internally, it is used `Object.assign` (no deep copy) to merge new data with older one!!!**
+     *  - It is also possible to register function to detect changes itself see examples
      * @example
+     *      // SIMPLE example
      *      const data_A= { a: "A" };
      *      const data_A_update= { a: "AAA" };
      *      const { add, mount, update }= $dom.component("UL", null);
-     *          add("LI", { onupdate: [ { a }, ({ a })=>({ textContent: a }) ] });//`[ { a },` add listener for "a"
+     *          add("LI", { onupdate: [ data_A, ({ a })=>({ textContent: a }) ] });//`[ { a },` add listener for "a"
      *      mount(document.body);
      *      update(data_A_update);
-     *      //BUT
+     *      // EXAMPLE WITH `mapUpdate`
      *      const data_B= { a: { b: "A" }};
      *      const data_B_update= { a: { b: "AAA" }};
      *      const { add, mount, update }= $dom.component("UL", null, { mapUpdate: d=>({ a: d.a.b }) });
-     *          add("LI", { onupdate: [ { a: data_B.a.b }, ({ a })=>({ textContent: a }) ] });//`[ { a },` add listener for "a"
+     *          add("LI", { onupdate: [ data_B, ({ a })=>({ textContent: a }) ] });//`[ { a },` add listener for "a" see `mapUpdate`
      *      mount(document.body);
      *      update(data_B_update);
+     *      // EXAMPLE WITH FUNCTION AS ARGUMENT OF `update`
+     *      const { add, mount, update }= $dom.component("UL", null, { mapUpdate: d=>({ a: d.a.b }) });
+     *          add("LI", { onupdate: [ { a: 1 }, ({ a })=>({ textContent: a }) ] });//`[ { a },` add listener for "a" see `mapUpdate`
+     *      mount(document.body);
+     *      update(({ a })=> { a: ++a });
      */
     function update(new_data){
         if(!internal_storage) return false;

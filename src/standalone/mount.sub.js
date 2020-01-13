a6b9882a08b3560abf25d085c5a551ee8625b6d2
Jan Andrle <andrle.jan@centrum.cz>
2020-01-13 11:04:22 +0100

    better parenNode in mount

--- src/standalone/mount.sub.js
+++ src/standalone/mount.sub.js
@@ -19,33 +19,39 @@
 function mount(element, type= "childLast"){
     if(observer) observer.disconnect();
     const component_el= !fragment.firstChild&&container ? container : fragment;
+    let parent_node;
     switch ( type ) {
         case "replace":
             $dom.replace(element, component_el);
+            parent_node= element.parentNode;
             break;
         case "replaceContent":
             $dom.empty(element);
             element.appendChild(component_el);
+            parent_node= element;
             break;
         case "before":
             element.parentNode.insertBefore(component_el, element);
+            parent_node= element.parentNode;
             break;
         case "after":
             $dom.insertAfter(component_el, element);
+            parent_node= element.parentNode;
             break;
         default:
             if(type==="childFirst" && element.childNodes.length) element.insertBefore(component_el, element.childNodes[0]);
             else element.appendChild(component_el);
+            parent_node= element;
             break;
     }
     observer= new MutationObserver(mutations=> mutations.forEach(function(record){
         if(!record.removedNodes||Array.prototype.indexOf.call(record.removedNodes, container)===-1) return false;
         destroy();
     }));
-    observer.observe(container.parentNode, { childList: true, subtree: true, attributes: false, characterData: false });
+    observer.observe(parent_node, { childList: true, subtree: true, attributes: false, characterData: false });
     if(on_mount_funs){
         on_mount_funs.forEach((onMountFunction, el)=> $dom.assign(el, onMountFunction.call(el, element, type)));
-        on_mount_funs= null;
+        on_mount_funs= undefined;
     }
     return container;
 }

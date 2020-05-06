b2a9d843f4a0763154d032309e48185756405d62
Jan Andrle <andrle.jan@centrum.cz>
2020-05-06 16:12:29 +0200

    jshint - maxparams: 5

--- src/_jaaJSU/$dom_add_helpers_standalone.js
+++ src/_jaaJSU/$dom_add_helpers_standalone.js
@@ -1,4 +1,4 @@
-/* jshint esversion: 6,-W097, -W040, browser: true, expr: true, undef: true, maxcomplexity: 19, maxparams: 4, maxdepth: 3 */
+/* jshint esversion: 6,-W097, -W040, browser: true, expr: true, undef: true, maxcomplexity: 19, maxparams: 5, maxdepth: 3 */
 /* $dom *//* global $dom */
 /* standalone= gulp_place("app.standalone=`standalone`", "variable"); */
 gulp_place("$dom_component.sub.js");/* global gulp_place */
